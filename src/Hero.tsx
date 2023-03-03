668d9bf7f292180a8d397a8a2dc36db5ac9cee5b
Kelsie Harris <kharris835@gmail.com>
2023-03-03 15:37:21 -0700

    feat: refactor hero navigation

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,28 +1,31 @@
-import React, { useState } from "react";
-import { Navigate } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Hero.css";
 
 export default function Hero() {
-  const [goToContactPage, setGoToContactPage] = useState(false);
-
-  if (goToContactPage) {
-    return <Navigate to="/contact" />;
-  }
+  const navigate = useNavigate();
 
   return (
     <div className="hero mt-5">
       <p className="mb-1">Hello! I'm</p>
       <h1 className="mb-0">Kelsie Harris</h1>
       <h2 className="mb-5">a front-end developer based in Colorado</h2>
-      <button
-        className="btn btn-branding"
-        title="Contact Page Link"
-        onClick={() => {
-          setGoToContactPage(true);
-        }}
-      >
-        Contact Me
-      </button>
+      <div className="d-flex gap-2 justify-content-center">
+        <button
+          className="btn btn-branding"
+          title="GitHub Link"
+          onClick={() => window.open("https://github.com/kharris835")}
+        >
+          GitHub
+        </button>
+        <button
+          className="btn btn-branding"
+          title="Contact Page Link"
+          onClick={() => navigate("/contact")}
+        >
+          Contact Me
+        </button>
+      </div>
     </div>
   );
 }
