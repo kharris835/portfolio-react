40adedc6f1a28628a42d988695adde18ef799f42
Kelsie Harris <kharris835@gmail.com>
2023-03-06 22:18:36 -0700

    feat: media query project cards

--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -1,20 +1,25 @@
 import React from "react";
+import "./ProjectCard.css";
 
 export default function ProjectCard(props) {
   return (
-    <div className="container p-5 bg-light my-5 rounded">
+    <div className="project-card container p-5 bg-light my-5 rounded">
       <div
-        className={`row d-flex gap-3 flex-wrap justify-content-between ${
+        className={`row d-flex gap-5 flex-wrap justify-content-between ${
           props.standardLayout ? "" : "flex-row-reverse"
         }`}
       >
-        <div className={`col text-${props.standardLayout ? "start" : "end"}`}>
+        <div
+          className={`col text-center text-lg-${
+            props.standardLayout ? "start" : "end"
+          }`}
+        >
           <h2 className="fs-1">{props.project.title}</h2>
           <p className="text-muted">{props.project.description}</p>
           <p>Built with {props.project.tools}</p>
           <a
             href={props.project.website}
-            className="btn btn-branding mt-2"
+            className="btn btn-branding mt-2 website-link"
             target="_blank"
             rel="noreferrer"
           >
@@ -22,7 +27,7 @@ export default function ProjectCard(props) {
           </a>
         </div>
         <div
-          className={`col d-flex justify-content-${
+          className={`col d-flex justify-content-center justify-content-lg-${
             props.standardLayout ? "end" : "start"
           }`}
         >
