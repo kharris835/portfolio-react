f010db34e0860331663ac8808bf1c248f6b8e58c
Kelsie Harris <kharris835@gmail.com>
2023-03-09 14:16:35 -0700

    chore: remove commented out lines

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -38,54 +38,5 @@ export default function NavBar() {
         </nav>
       </div>
     </div>
-
-    // <nav className="navbar navbar-expand-lg bg-white fixed-top">
-    //   <div className="container-fluid">
-    //     <a className="navbar-brand" href="/">
-    //       <img
-    //         src="images/logo.png"
-    //         alt="Flower logo"
-    //         className="logo d-none d-lg-block"
-    //         title="Homepage"
-    //       />
-    //       <img
-    //         src="images/icon.png"
-    //         alt="Flower icon"
-    //         className="logo d-block d-lg-none"
-    //         title="Homepage"
-    //       />
-    //     </a>
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="collapse"
-    //       data-bs-target="#navbarNav"
-    //       aria-controls="navbarNav"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //     >
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-    //     <div className="collapse navbar-collapse" id="navbarNav">
-    //       <ul className="navbar-nav">
-    //         <li className="nav-item">
-    //           <a className="nav-link active" aria-current="page" href="/">
-    //             About
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link" href="/">
-    //             Projects
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link" href="/">
-    //             Contact
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
   );
 }
