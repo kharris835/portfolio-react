e0bb5240fcdfdcf993e7edc2a78c22573457a6d5
Kelsie Harris <kharris835@gmail.com>
2023-03-08 21:22:19 -0700

    style: contact media query

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -4,47 +4,45 @@ import "./Contact.css";
 export default function Contact() {
   return (
     <div className="contact mt-5">
-      <div className="container content-container">
-        <div className="content">
-          <h2 className="contact-eyebrow mb-3">Let's work together!</h2>
-          <h1 className="mb-5">Contact Me</h1>
-          <h3>Email</h3>
-          <p className="text-muted contact-link">
-            <a
-              href="mailto:kharris835@gmail.com"
-              target="_blank"
-              rel="noreferrer"
-              className="contact-link"
-              title="Email Kelsie"
-            >
-              kharris835@gmail.com
-            </a>
-          </p>
-          <h3>GitHub</h3>
-          <p className="text-muted contact-link">
-            <a
-              href="https://github.com/kharris835"
-              target="_blank"
-              rel="noreferrer"
-              className="contact-link"
-              title="GitHub Profile"
-            >
-              kharris835
-            </a>
-          </p>
-          <h3>LinkedIn</h3>
-          <p className="text-muted contact-link">
-            <a
-              href="https://www.linkedin.com/in/kelsieharris/"
-              target="_blank"
-              rel="noreferrer"
-              className="contact-link"
-              title="LinkedIn Profile"
-            >
-              kelsieharris
-            </a>
-          </p>
-        </div>
+      <div className="content">
+        <h2 className="contact-eyebrow mb-3">Let's work together!</h2>
+        <h1 className="mb-5">Contact Me</h1>
+        <h3>Email</h3>
+        <p className="text-muted">
+          <a
+            href="mailto:kharris835@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            className="contact-link"
+            title="Email Kelsie"
+          >
+            kharris835@gmail.com
+          </a>
+        </p>
+        <h3>GitHub</h3>
+        <p className="text-muted">
+          <a
+            href="https://github.com/kharris835"
+            target="_blank"
+            rel="noreferrer"
+            className="contact-link"
+            title="GitHub Profile"
+          >
+            kharris835
+          </a>
+        </p>
+        <h3>LinkedIn</h3>
+        <p className="text-muted">
+          <a
+            href="https://www.linkedin.com/in/kelsieharris/"
+            target="_blank"
+            rel="noreferrer"
+            className="contact-link"
+            title="LinkedIn Profile"
+          >
+            kelsieharris
+          </a>
+        </p>
       </div>
     </div>
   );
